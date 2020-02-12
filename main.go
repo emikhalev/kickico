14cd57587fd525e62c94281f431b7ec5f756d02d
Eugene Mikhalev <emikhalev@ozon.travel>
2020-02-12 17:47:10 +0300

    in progress

--- main.go
+++ main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io/ioutil"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+
+	"github.com/emikhalev/kickio/contracts/cstoken"
 )
 
 const (
+	contractABI = "contracts/CSToken.json"
 	SmartContractAddr = "0x27695e09149adc738a978e9a678f99e4c39e9eb9"
 )
 
 func main() {
-	client, err := ethclient.Dial("mainnet.infura.io/v3/8f81eca071e245e6934ad24b23c722d0")
+	/*_, err := ethclient.Dial("mainnet.infura.io/v3/8f81eca071e245e6934ad24b23c722d0")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("we have a connection")
+	fmt.Println("we have a connection")*/
 
-	// Loading smart contract
-	address := common.HexToAddress(SmartContractAddr)
-	instance, err := store.NewStore(address, client)
-	if err != nil {
+	abiContent, err := ioutil.ReadFile(contractABI)
+	if err!=nil {
 		log.Fatal(err)
 	}
-	_ = instance
+
+	cABI, err := abi.JSON(bytes.NewReader(abiContent))
+	if err!=nil {
+		log.Fatal(err)
+	}
+	for mName := range cABI.Methods {
+		println(mName)
+	}
+	// Loading smart contract
+	//address := common.HexToAddress(SmartContractAddr)
+	//instance, err := store.NewStore(address, client)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//_ = instance
 
 }
